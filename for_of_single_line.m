79c15aa232c146df1014ded1c08e38a016442b10
wens <wens.wq@gmail.com>
2015-01-05 11:10:21 +0800

    Imported user code snippets

---
+++ for_of_single_line.m
@@ -0,0 +1,11 @@
+// for of single line
+// 
+//
+// IDECodeSnippetCompletionPrefix: sfor
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: B325441A-8FAF-4CF2-9A1A-A0FB8C2031F6
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+for (<#initialization#>; <#condition#>; <#increment#>)
+    <#statements#> 
